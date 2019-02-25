daf33d9ae8568f4bacb285f48f5bf64035a7d33f
Amanda-Kolopa <19229712+Amanda-Kolopa@users.noreply.github.com>
2019-02-24 22:12:21 -0500

    Update HelloWorldTest.php

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -2,10 +2,32 @@
 
 class HelloWorldTest extends \PHPUnit_Framework_TestCase
 {
-  public function testTrueAssertsToTrue()
+  public function SampleTest()
   {
-    $this->assertTrue(true);
+    global $object;
+    $this->object = new \tests\ObjectTest;
   }
+
+  public function testGetTheContentFromFeed()
+  {
+    //$this->assertTrue(true); //checks with assetsTrue to see if param is true
+
+     $object = new \tests\ObjectTest;
+
+     $object->setUsernameOfLoggedInUser('Updated');
+
+     echo "setUsernameofLoggedInUser: ".$object;
+
+     $object->getUsernameOfLoggedInUser();
+
+     echo "getUsernameofLoggedInUser: ".$object;
+
+     $object->setUsernameOfLoggedInUser('Updated Again');
+
+     echo "setUsernameofLoggedInUser: ".$object;
+
+     $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
+  } 
 }
 
 ?>
